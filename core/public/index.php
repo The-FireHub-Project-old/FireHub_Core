d7f5e4bde34a58db52457278db354db480e34e49
Riddick <danijel.galic@live.com>
2022-06-14 08:58:44 +0200

    Created public landing files for PHAR
    Two files, one for landing with console, and one for
    landing with browser.

---
+++ core/public/index.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types = 1);
+
+/**
+ * Main Core web public landing file
+ *
+ * This is a landing file if PHAR archive is accessed
+ * through a web browser.
+ * @since 0.1.2.pre-alpha.M1
+ *
+ * @author Danijel Galić
+ * @copyright 2022 FireHub Web Application Framework
+ * @license OSL Open Source License version 3 - [https://opensource.org/licenses/OSL-3.0](https://opensource.org/licenses/OSL-3.0)
+ *
+ * @package FireHub\Public
+ * @version 1.0
+ */
