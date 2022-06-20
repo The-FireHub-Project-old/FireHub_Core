2618e30576769e0b9baa02f8beecf58c910079a1
Riddick <danijel.galic@live.com>
2022-06-20 14:31:02 +0200

    Created abstract kernel (#7)
    Process requests that come in through various sources
    and give client appropriate response.

---
+++ core/initializers/firehub.Kernel.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of FireHub Web Application Framework package.
+ * @since 0.1.6.pre-alpha.M1
+ *
+ * @author Danijel Galić
+ * @copyright 2022 FireHub Web Application Framework
+ * @license OSL Open Source License version 3 - [https://opensource.org/licenses/OSL-3.0](https://opensource.org/licenses/OSL-3.0)
+ *
+ * @package FireHub\Initializers
+ * @version 1.0
+ */
+
+namespace FireHub\Initializers;
+
+/**
+ * ### Abstract Base Kernel
+ *
+ * Process requests that come in through various sources
+ * and give client appropriate response.
+ * @since 0.1.6.pre-alpha.M1
+ *
+ * @package FireHub\Initializers
+ */
+abstract class Kernel {
+
+    /**
+     * ### Handle client runtime
+     * @since 0.1.6.pre-alpha.M1
+     *
+     * @return string
+     */
+    abstract public function runtime ():string;
+
+}
